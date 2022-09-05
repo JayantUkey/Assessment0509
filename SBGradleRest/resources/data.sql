fbef64d8297fc3559ab63008a27bac3b29bd6429
JayantUkey <110605408+JayantUkey@users.noreply.github.com>
2022-09-05 10:44:06 +0530

    Create data.sql

---
+++ SBGradleRest/resources/data.sql
@@ -0,0 +1,6 @@
+insert into employee values (1,'Jayant','Java');
+insert into employee values (2,'Sarvesh','Android');
+insert into employee values (3,'Nikhil','Python');
+insert into employee values (4,'Shivam','Full Stack');
+
+ 
