65506211e2dd27e1311fed793028e35e864e10c7
JayantUkey <110605408+JayantUkey@users.noreply.github.com>
2022-09-05 10:55:56 +0530

    Create SbSwaggerSqlApplication.java

---
+++ SBSwaggerApi/java/main/SbSwaggerSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSwaggerSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSwaggerSqlApplication.class, args);
+	}
+
+}
